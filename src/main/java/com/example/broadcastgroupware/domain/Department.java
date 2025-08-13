9babca1cbbd6e4004534235b4e7d29f9fd290586
kim <smkim000721@naver.com>
2025-08-13 17:28:02 +0900

    [김성민] 도메인 등록

---
+++ src/main/java/com/example/broadcastgroupware/domain/Department.java
@@ -0,0 +1,10 @@
+package com.example.broadcastgroupware.domain;
+
+import lombok.Data;
+
+@Data
+public class Department {
+	private int departmentId;
+	private String departmentName;
+	private String createDate;
+}
