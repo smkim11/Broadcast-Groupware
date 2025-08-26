da7ab638f7fe9df46751d4739efd3cdb2ab5f2f1
kim <smkim000721@naver.com>
2025-08-26 16:57:10 +0900

    [김성민] 당일 근태기록 조회

--- src/main/java/com/example/broadcastgroupware/controller/AttendanceController.java
+++ src/main/java/com/example/broadcastgroupware/controller/AttendanceController.java
@@ -1,20 +1,33 @@
 package com.example.broadcastgroupware.controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.broadcastgroupware.domain.Attendance;
+import com.example.broadcastgroupware.dto.UserSessionDto;
+import com.example.broadcastgroupware.service.AttendanceService;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class AttendanceController {
+	private AttendanceService attendanceService;
+	public AttendanceController(AttendanceService attendanceService) {
+		this.attendanceService = attendanceService;
+	}
+	
 	// 본인 근태
 	@GetMapping("/attendance")
-	public String attendance(Model model) {
-		LocalDateTime now = LocalDateTime.now();
-		String formattedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+	public String attendance(Model model, HttpSession session) {
+		UserSessionDto user = (UserSessionDto)session.getAttribute("loginUser");
+		Attendance attendance = attendanceService.selectUserAttendance(user.getUserId());
+		if(attendance != null) {
+			model.addAttribute("attendance",attendance);
+		}
 		
+		System.out.println(attendance);
+		model.addAttribute("loginUser",user.getUserId());
 	    return "user/attendance";  
 	}
 }
