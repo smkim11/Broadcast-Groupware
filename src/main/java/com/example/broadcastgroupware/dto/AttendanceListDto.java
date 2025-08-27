80d7c50f48df435340049f8ac9b00d4186a212d4
kim <smkim000721@naver.com>
2025-08-27 16:02:10 +0900

    [김성민] 직책별 직원근태조회용 Dto 추가

---
+++ src/main/java/com/example/broadcastgroupware/dto/AttendanceListDto.java
@@ -0,0 +1,10 @@
+package com.example.broadcastgroupware.dto;
+
+import lombok.Data;
+
+@Data
+public class AttendanceListDto {
+	private int userId;
+	private String role;
+	private String userRank;
+}
