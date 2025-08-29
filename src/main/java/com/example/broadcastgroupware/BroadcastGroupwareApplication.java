3a7a8bb6010697f288d76ba9d0d97c94b9dec942
kim <smkim000721@naver.com>
2025-08-29 11:13:08 +0900

    [김성민] WebMvcConfigurer 추가

--- src/main/java/com/example/broadcastgroupware/BroadcastGroupwareApplication.java
+++ src/main/java/com/example/broadcastgroupware/BroadcastGroupwareApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
-public class BroadcastGroupwareApplication {
+public class BroadcastGroupwareApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BroadcastGroupwareApplication.class, args);
 	}
+	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/final/**").addResourceLocations("file:///C:/final/");
 	}
