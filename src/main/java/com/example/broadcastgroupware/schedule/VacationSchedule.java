67124fbd7875a1beebdb03a5da39a4f92c9630f3
kim <smkim000721@naver.com>
2025-09-02 17:20:53 +0900

    [김성민] 함수명 변경

--- src/main/java/com/example/broadcastgroupware/schedule/VacationSchedule.java
+++ src/main/java/com/example/broadcastgroupware/schedule/VacationSchedule.java
@@ -19,7 +19,7 @@ public class VacationSchedule {
 	
 	// 매년 1월1일에 전년도에 일한 개월수 만큼 연차 추가
 	@Scheduled(cron = "0 0 0 1 1 *")
-	public void HolidaySchedule() {
+	public void VacationSchedule() {
 		List<Integer> userList = vacationService.selectJoinUser();
 		for(int userId:userList) {
 			vacationService.updateVacation(userId);
